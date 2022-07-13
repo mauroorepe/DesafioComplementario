858b76b8fc4af1e8940d58922f836472ebca7efa
Mauro <mauro.repetto@hotmail.com>
2022-07-13 14:02:29 -0300

    primer desafio

---
+++ js/complementario.js
@@ -0,0 +1,31 @@
+//PROPUESTO 1
+let numero= parseInt(prompt("Ingrese un numero"));
+let numero2= parseInt(prompt("Ingrese otro numero"));
+
+while(numero!="X" && numero2!="X"){
+    let resultado = numero + numero2;
+    alert(resultado)
+    numero = parseInt(prompt("Ingrese un numero"));
+    numero2= parseInt(prompt("Ingrese otro numero"));
+}
+
+
+//PROPUESTO 2
+
+/* let palabra = prompt("");
+let palabra2= prompt("");
+
+while(palabra!="ESC" && palabra2!="ESC" ){
+    alert(palabra+=" "+palabra2);
+    palabra2=prompt("");
+} */
+
+
+
+//PROPUESTO 3
+
+/* let numero = parseInt(prompt("Ingrese un numero"));
+
+for(let i=0; i<numero; i++){
+    alert((i+1)+ " " + "Hola");
+} */
